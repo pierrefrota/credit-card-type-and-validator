b1352bba86931e856fbb358f1c42651365d79c1f
Pierre Frota <pierre@focomultimidia.com>
2022-12-04 20:29:48 -0300

    First commit

--- src/pages/api/v1/valid/index.ts
+++ src/pages/api/v1/valid/index.ts
@@ -3,14 +3,28 @@ import { checkIfCardIsValid } from "../../../../utils/checkIfCardIsValid";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
   if (req.method !== "GET") {
-    return res.status(401).json({
+    return res.status(405).json({
       message: "Method not allowed.",
       error: true,
     });
   }
 
+  if (!req.headers.token) {
+    return res.status(401).json({
+      error: true,
+      message: "Token not provided.",
+    });
+  }
+
+  if (req.headers.token !== process.env.TOKEN) {
+    return res.status(401).json({
+      error: true,
+      message: "Your token is invalid.",
+    });
+  }
+
   if (!req.query.card) {
-    return res.json({
+    return res.status(400).json({
       error: true,
       message: "Card number is required.",
     });
